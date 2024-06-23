02fcfc8def6ef7317c4bdada0b1235dfb958357f
Affan Imran <affanimran@Affans-Air.lan>
2024-06-23 17:28:48 +0400

    pls deploy

--- backend/index.js
+++ backend/index.js
@@ -59,7 +59,7 @@ app.post("/api/upload", upload.single("file"), (req, res) => {
   }
 });
 
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
   connectDB();
--- frontend/src/pages/PostDetails.jsx
+++ frontend/src/pages/PostDetails.jsx
@@ -44,8 +44,8 @@ function PostDetails() {
     try {
       await axios.delete(`${URL}/api/posts/${id}`, { withCredentials: true });
       console.log("Post has been deleted");
-      navigate("http://localhost:5173/");
-      // navigate("/");
+      // navigate("http://localhost:5173/");
+      navigate("/");
     } catch (error) {
       console.log(error);
     }
